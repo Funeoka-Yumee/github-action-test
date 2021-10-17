ffcd841419a7acc84d81325a29aa339eeb55df93
Sen Yang <syang15@fudan.edu.cn>
2021-10-17 19:22:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import time
 
 os.system('dig a6008.com A @120.79.177.7')
 
-for i in range(2000):
+for i in range(2500):
     random_str = ''.join([random.choice(string.ascii_letters+string.digits) for i in range(6)])
     domain = '{}.a6008.com'.format(random_str)
     os.system(f'dig {domain} A @120.79.177.7')
